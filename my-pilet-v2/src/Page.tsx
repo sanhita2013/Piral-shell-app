dcefeea5e033456557be104f3674161c856ea635
sanhita <sanhita.10721130@ltimindtree.com>
2024-09-20 19:52:54 +0530

    piral_shell_app

---
+++ my-pilet-v2/src/Page.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+export default () => {
+  return (
+    <>
+      <h1>This is from pilet version 2</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat reiciendis illum qui nulla adipisci, laudantium
+        vitae atque dicta blanditiis ab. Quae deleniti eum sapiente temporibus tenetur, maxime quibusdam iure nisi.
+        <Link to="/page1">Go to Pilet 1</Link>
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda adipisci autem et nam quis, corporis
+        maxime id? Commodi maiores eligendi similique velit porro consectetur! Voluptates quo autem omnis deserunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis similique veritatis nesciunt non esse commodi
+        eligendi fugit officia earum distinctio. Mollitia blanditiis iusto cum pariatur quaerat fuga qui molestias
+        delectus?
+      </p>
+    </>
+  );
+};
